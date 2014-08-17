d13a8c21c8c03c9f6a87363fa5ba9daae51550b9
faiweiner <faiweiner@gmail.com>
2014-08-17 21:15:24 +1000

    finished User model FactoryGirl add

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -55,11 +55,11 @@ RSpec.describe User, :type => :model do
 			it 'should be valid' do
 				@user = FactoryGirl.create(:user)
 				addresses = %w[user@foo.COM A_US-ER@f.b.org frst.lst@foo.jp a+b@baz.cn]
-	      addresses.each do |valid_address|
-	        @user.email = valid_address
-	        expect(@user).to be_valid
-	      end
-	   		expect(@user).to be_valid
+				addresses.each do |valid_address|
+					@user.email = valid_address
+					expect(@user).to be_valid
+				end
+				expect(@user).to be_valid
 			end
 		end
 		
@@ -82,43 +82,68 @@ RSpec.describe User, :type => :model do
 		it 'should have a username' do
 			expect(FactoryGirl.create(:user).username).to eq FactoryGirl.attributes_for(:user)[:username]
 		end
-		# it 'shold have a unique username' do
-		# 	expect(@user4).to validate_uniqueness_of(:username)
-		# end
 
-		# context 'when username is too short' do
-		# 	it { expect(@user6).to be_invalid }
-		# end
+		it 'shold have a unique username' do
+			expect(FactoryGirl.create(:user)).to validate_uniqueness_of(:username)
+		end
 
-		# context 'when username is a duplicate' do
-		# 	it { expect(@user4).to be_invalid }
-		# end
+		context 'when username is not unique' do
+			it 'should be invalid' do
+				user = FactoryGirl.create(:user)
+				new_user = FactoryGirl.build(:user, email: 'new_user@email.com')
+				expect(new_user).to be_invalid
+			end
+		end
 
-		# context 'when username is blank' do
-		# 	it { expect(@user3).to be_invalid }
-		# end
+		context 'when username is too short' do
+			it 'should be invalid' do
+				expect(FactoryGirl.build(:user, username: 'short')).to be_invalid
+			end
+		end
+
+		context 'when username is blank' do
+			it 'should be invalid' do
+				expect(FactoryGirl.build(:user, username: '')).to be_invalid
+			end
+		end
 	end
 
-	# # password
-	# describe 'password field' do
-	# 	it 'should have a password' do
-	# 		expect(@user1.password).to eq 'chicken'
-	# 		expect(@user1.password_confirmation).to eq 'chicken'
-	# 	end
+	# password
+	describe 'password field' do
+		it 'should have a password' do
+			@user = FactoryGirl.create(:user)
+			expect(@user.password).to eq 'chicken'
+			expect(@user.password_confirmation).to eq 'chicken'
+		end
 
-	# 	it 'should be between 6 and 20 characters' do
-	# 		passwords = ['aaa', 'aaaaaaaaaaaaaaaaaaaaaaaa']
-	# 		passwords.each do |invalid_password|
-	# 			@user7.password = invalid_password
-	# 			@user7.password_confirmation = invalid_password
-	# 			expect(@user7).to be_invalid
-	# 		end
-	# 	end
-	# 	context 'when password is not present' do
-	# 		it { expect(@user8).to be_invalid }
-	# 	end
+		context 'when password is between 6 and 20 characters' do
+			it 'should be valid' do
+				@user = FactoryGirl.create(:user)
+				expect(@user).to be_valid
+			end
+		end
 
-	# 	context 'when passwords don\'t match' do
-	# 		it { expect(@user7).to be_invalid }
-	# 	end
+		context 'when password is less than 6 or more than 20 characters' do
+			it 'should be invalid' do
+				@user = FactoryGirl.create(:user)
+				passwords = ['aaa', 'aaaaaaaaaaaaaaaaaaaaaaaa']
+				passwords.each do |invalid_password|
+					@user.password = invalid_password
+					@user.password_confirmation = invalid_password
+					expect(@user).to be_invalid
+				end
+				expect(@user).to be_invalid
+			end
+		end
+
+		context 'when password is not present' do
+		  it 'should be invalid' do
+		  	expect(FactoryGirl.build(:user, password: '', password_confirmation: '')).to be_invalid
+		  end
+		end
+
+		context 'when passwords don\'t match' do
+		  it { expect(FactoryGirl.build(:user, password: 'turkey', password_confirmation: 'chicken')).to be_invalid }
+		end
+	end
 end
