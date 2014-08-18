1e7ee6d2cfa50187e38a824612675fe22cf40f84
faiweiner <faiweiner@gmail.com>
2014-08-18 23:01:02 +1000

    custom flash notice for successful league creation

--- app/controllers/leagues_controller.rb
+++ app/controllers/leagues_controller.rb
@@ -11,7 +11,15 @@ class LeaguesController < ApplicationController
 	def create
 		@league = League.new league_params
 		if @league.save
-			flash[:notice] = "You've successfully signed up."
+
+			# get customized text based on type
+			@access_type = nil
+			if @league.public_access == true
+				@access_type = "public"
+			else
+				@access_type = "private"
+			end
+			flash[:notice] = "You've successfully created a #{access_type} league!"
 			# Once someone signs up, they currently need to log in. Better to have automatically log-in?
 			flash[:color] = "valid"
 			redirect_to league_path(League.last)
