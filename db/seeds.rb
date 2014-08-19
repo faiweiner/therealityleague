9a9dc197f1dfb71ee35e98226367ecc27bd2b62a
faiweiner <faiweiner@gmail.com>
2014-08-19 16:53:31 +1000

    rerake

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,10 +9,10 @@ user3 = User.create(:email => 'user1@gmail.com', :username => 'username1', :avat
 user4 = User.create(:email => 'user2@gmail.com', :username => 'username2', :avatar => 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png', :password => 'chicken', :password_confirmation => 'chicken')
 user5 = User.create(:email => 'user3@gmail.com', :username => 'username3', :avatar => 'http://png-1.findicons.com/files/icons/1072/face_avatars/300/i04.png', :password => 'chicken', :password_confirmation => 'chicken')
 
-show1 = Show.create(:name => 'The Bachelor', :premiere_date => '02/03/14', :draft_close_date => '06/03/14')
-show2 = Show.create(:name => 'The Bachelorette', :premiere_date => '02/03/14', :draft_close_date => '06/03/14')
-show3 = Show.create(:name => 'The Challenge', :premiere_date => '02/03/14', :draft_close_date => '06/03/14')
-show4 = Show.create(:name => 'The Voice', :premiere_date => '02/03/14', :draft_close_date => '06/03/14')
+show1 = Show.create(:name => 'The Bachelor', :premiere_date => '14/03/06', :draft_close_date => '14/03/06')
+show2 = Show.create(:name => 'The Bachelorette', :premiere_date => '14/03/02', :draft_close_date => '14/03/02')
+show3 = Show.create(:name => 'The Challenge', :premiere_date => '14/03/02', :draft_close_date => '14/03/02')
+show4 = Show.create(:name => 'The Voice', :premiere_date => '14/03/02', :draft_close_date => '14/03/02')
 
 league1 = League.create(:name => 'The Best Public League', :commissioner_id => user1.id, :show_id => show3.id, :public_access => true)
 league2 = League.create(:name => 'The Super Private League', :commissioner_id => user2.id, :show_id => show3.id, :public_access => false)
