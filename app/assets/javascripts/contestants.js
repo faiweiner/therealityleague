61ae41de9a501dee4c14c3d34e30347fca405f00
faiweiner <faiweiner@gmail.com>
2014-08-27 00:07:17 +1000

    got superuser popup edit working

--- app/assets/javascripts/contestants.js
+++ app/assets/javascripts/contestants.js
@@ -1,10 +1,14 @@
 $(document).ready(function () {
 	console.log('Contestants initialized');
 
+	$.fn.editable.defaults.mode = 'popup';
+	$('#username').editable();
 	// ========= universal click listener ========= //
 	$('.edit').on('click', function (event) {
 		$element = event.target
-		$element.contentEditable = "true";
+		$element.contentEditable('/contestants/update/', {
+
+		});
 
 		console.log($element);
 	});
