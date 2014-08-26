8aed8af17b510fd7e2c82c7e27f793cead65611e
faiweiner <faiweiner@gmail.com>
2014-08-26 16:33:50 +1000

    new admin page for shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -2,7 +2,18 @@ class ShowsController < ApplicationController
 	def index
 		@shows = Show.where(:expired => :false).order("premiere_date ASC")
 		@past_shows = Show.where(:expired => :true).order("premiere_date DESC")
+		@current_date = DateTime.now.strftime("%B %d, %Y")
+
 	end
+
+	def new
+		@shows = Show.all
+	end
+
+	def edit
+		@show = Show.find(params[:id])
+	end
+
 	def show
 		@show = Show.find(params[:id])
 		@contestants = @show.contestants
