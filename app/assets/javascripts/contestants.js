be21573685dbb7edc896d3d44df4be22942fcb49
Fai Weiner <fai.weiner@gmail.com>
2014-08-27 13:31:30 +1000

    loading ajax now but info not posting

--- app/assets/javascripts/contestants.js
+++ app/assets/javascripts/contestants.js
@@ -12,6 +12,9 @@ $(document).ready(function () {
 			return false;
 		}    
 	};
+
+	$('#contestants').find('.btn-primary').hide();
+
 	$.extend($.fn.editable.defaults, defaults);
 
 	$('#contestants span[data-name="name"]').editable({
@@ -26,7 +29,6 @@ $(document).ready(function () {
 	$('#contestants span[data-name="occupation"]').editable({
 		title: 'Enter occupation',
 		tpl: "<input style='width: 100px'>",
-		type: 'textarea'
 	});
 
 $('#contestants span[data-name="description"]').editable({
@@ -34,23 +36,39 @@ $('#contestants span[data-name="description"]').editable({
 		type: 'textarea'
 	});
 
-	$('#contestants').on('click', '.edit', function(){
-			$('#contestants').find('.editable-open').editable('hide');
-			$('#contestants').find('.btn-primary').hide();
-			$('#contestants').find('.edit').show();
-			$(this).hide().siblings('.btn-primary').show();
-			$(this).closest('tr').find('.editable').editable('show');
+	$('#contestants').on('click', '.edit', function (){
+		$('#contestants').find('.editable-open').editable('hide');
+		$('#contestants').find('.btn-primary').hide();
+		$('#contestants').find('.edit').show();
+		$(this).hide().siblings('.btn-primary').show();
+		$(this).closest('tr').find('.editable').editable('show');
 	});
 
 	$('#contestants').on('click', '.btn-primary', function() {
-			var $btn = $(this);
-			/*
-			 Currently no elegant way to get actual values from shown inputs.
-			 It's possible to collect it manually, submit and then use `setValue` method to update data in table row. But it's overload..
-			 Need investigation.
-			*/
-			$btn.closest('tr').find('.editable').editable('hide');
-			$btn.hide().siblings('.edit').show();
+		var $btn = $(this);
+		/*
+		 Currently no elegant way to get actual values from shown inputs.
+		 It's possible to collect it manually, submit and then use `setValue` method to update data in table row. But it's overload..
+		 Need investigation.
+		*/
+		var contestantId = jQuery(this).closest('tr').find('span')[0].dataset.pk
+		$.ajax({
+			url: '/contestants/' + contestantId,
+			type: 'POST',
+			responseTime: 200,
+			response: function(settings) {
+				console.log(settings.data);   
+		 	}
+			// success: function (msg) {
+			// 	var partial = msg;
+			// 	$rosterBoard.html(partial);
+			// }
+		});
+
+		console.log('got here');
+
+		$btn.closest('tr').find('.editable').editable('hide');
+		$btn.hide().siblings('.edit').show();
 	});
 
 	// //ajax emulation
