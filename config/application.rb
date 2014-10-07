d5d14cffe2226eb6bc0dc8bdba2f954cc1c7eb31
fai.weiner <fai.weiner@gmail.com>
2014-10-07 16:03:57 +1100

    added config.autoload_paths += %W(#{config.root}/app/models/*) to config.application to create subfolders for better organization

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,8 @@ module RealityApp
 	# Settings in config/environments/* take precedence over those specified here.
 	# Application configuration should go into files in config/initializers
 	# -- all .rb files in that directory are automatically loaded.
+	config.autoload_paths += %W(#{config.root}/app/models/leagues)
+	config.autoload_paths += %W(#{config.root}/app/models/schemes)
 
 	# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
 	# Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
