b0950182efdcead64fc9caadd02e0e5a27d08be5
fai.weiner <fai.weiner@gmail.com>
2014-10-08 18:10:24 +1100

    events js got through first step with second step populated but not specified

--- app/assets/javascripts/events.js
+++ app/assets/javascripts/events.js
@@ -2,66 +2,64 @@ $(document).ready(function () {
 	$eventCreationDiv = $('#event_create');
 
 	if ($eventCreationDiv.length > 0) {
-		$.ajax({
-			url: 				'/api/shows',
-			data: 			{format: 'js'},
-			dataType: 	'json',
-			success: function (data) {
-				$exportShows = $.map(data.exportShows, function (item, index) {
-					return {
-						label: item.name,
-						value: item.id
-					}
-				});
-				$eventCreationDiv.cascadingDropdown({
-					selectBoxes: [
-						{
-							selector: '.step1',
-							source: $exportShows,
-							onChange: function (event, value, requiredValue) {
-								console.log(value);
-								selectBoxes: [
-									{
-										selector: '.step2',
-										requires: ['.step1'],
-										paranName: 'showId',
-										source: function (request, response) {
-											$.getJSON('/api/seasons', request, function (data) {
-												var selectOnlyOption = data.length <= 1;
-												debugger
-												response($.map(data.exportSeasons, function (item, index) {
-													return {
-														label: item.name,
-														value: item.id,
-														selected: selectOnlyOption
-													}
-												})); // end response
-											}) // end .getJSON request
-										}
-									}
-								] // end second selectBoxes
-							} // end onChange function
-						}, 		// end step1
-						{
-							selector: '.step2',
-							requires: ['.step1'],
-							paramName: 'showId',
-							source: function (request, response) {
-								$.getJSON('/api/seasons', request, function (data) {
-									var selectOnlyOption = data.length <= 1;
-									response($.map(data.exportSeasons, function (item, index) {
-										return {
-												label: item.name,
-												value: item.id,
-												selected: selectOnlyOption // Select if only option
-										};
-									}));
-								}); // end .getJSON
+		$eventCreationDiv.cascadingDropdown({
+			selectBoxes: [
+				{
+					selector: '.step1',
+					source: function(request, response) {
+						$.getJSON('/api/shows', request, function (data) {
+							response($.map(data.exportShows, function (item, index) {
+								return {
+									label: item.name,
+									value: item.id
+								}
+							}));
+						});
+					},
+					onChange: function (event, value, requiredValues) {
+						console.log(value);
+						console.log(requiredValues);
+						selectBoxes: [
+							{
+								selector: '.step2',
+								requires: ['.step1'],
+								paramName: 'showId',
+								source: function (request, response) {
+									$.getJSON('/api/seasons', request, function (data) {
+										var selectOnlyOption = data.length <= 1;
+										response($.map(data.exportSeasons, function (item, index) {
+											return {
+													label: item.name,
+													value: item.id,
+													selected: selectOnlyOption // Select if only option
+											};
+										}));
+									});
+								}
 							}
-						}		// end step2
-					] // end selectBoxes
- 				}) // end cascadingDropDown
-			} // end ajax.success request
-		}); // end AJAX request
+						]
+					}
+				},
+				{
+					selector: '.step2',
+					requires: ['.step1'],
+					paramName: 'showId',
+					source: function (request, response) {
+						$.getJSON('/api/seasons', request, function (data) {
+							var selectOnlyOption = data.length <= 1;
+							response($.map(data.exportSeasons, function (item, index) {
+								return {
+										label: item.name,
+										value: item.id,
+										selected: selectOnlyOption // Select if only option
+								};
+							}));
+						});
+					},
+					onChange: function (event, value, requiredValues) {
+					}
+				}
+			]
+		});
 	}; // end IF statement $eventCreationDiv
 });
