100d2da6e8aea9c4ce040487e385cc4bba9dbeb5
fai.weiner <fai.weiner@gmail.com>
2015-01-07 23:11:19 +1100

    feedback feature working now yes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -35,16 +35,32 @@ console.log('X-Editable defaults initialized');
 
 $(document).ready(function () {
 
-	var clearField = function (fieldId) {
-		var targetfieldId = '#' + fieldId;
-		var targetField = $(targetfieldId);
+	var clearField = function (targetField) {
 		targetField.val('');
+	};
+
+	var	hideFormGroup = function (formGroupId) {
+		$formGroup = $(formGroupId);
+		$formGroup.hide();
+	};
+	var showFormGroup = function (formGroupId) {
+		$formGroup = $(formGroupId);
+		$formGroup.show();
+	};
+
+	var addAlertClass = function (formGroupId, status) {
+		if (status == "success") {
+			$targetElement = $(formGroupId)
+		} else if (status == "error") {
+
+		}
 	};
 
 	if ($('#feedbackBox')) {
 		$('#new_message').submit(function (e) {
 			var formData = $(this).serializeArray();
 			var responseMessage;
+
 			$.ajax({
 				url : '/messages',
 				type: 'POST',
@@ -59,19 +75,35 @@ $(document).ready(function () {
 				failure: function (data, jqXHR, textStatus) {
 				}
 			}).done(function (data){
-				$responseBox = $('#post-response-box')
+				$responseBox = $('#post-response-box');
+				$responseBox.show();
 				$responseBox.addClass('alert-success');
+				if (data.success) {
+					addAlertClass('#post-response-box', 'success');
+				} else if (data.error) {
+					addAlertClass('#post-response-box', 'error');
+				}
 				var successMessage = data.success;
 				$responseBox.text(data.success);
 
-				clearField('message_user_id');
-				clearField('message_messagetype');
-				clearField('message_messagecomment');
-								
+				clearField($('#message_messagecomment'));
+
+				// $responseBox.text('').delay(5000);
+				// $responseBox.hide().delay(1000);
 			});
 
 
 			e.preventDefault(); //STOP default action
+		});
+
+		$('.close').on('click', function () {
+			$('#post-response-box').text('');
+			$('#post-response-box').hide();
+		});
+
+		$('#form-cancel-button').on('click', function () {
+			$('#post-response-box').text('');
+			$('#post-response-box').hide();
 		});
 	};
 });
