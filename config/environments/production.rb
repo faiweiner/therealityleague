05d5969feec34ddabfcd6f9fc027b2b7bf0644b1
fai.weiner <fai.weiner@gmail.com>
2015-01-18 18:44:46 +1100

    updated production config to store cache and compile assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,14 +51,14 @@ Rails.application.configure do
 	# config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
 	# Use a different cache store in production.
-	# config.cache_store = :mem_cache_store
+	config.cache_store = :mem_cache_store
 
 	# Enable serving of images, stylesheets, and JavaScripts from an asset server.
 	# config.action_controller.asset_host = "http://assets.example.com"
 
 	# Precompile additional assets.
 	# application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-	# config.assets.precompile += %w( search.js )
+	config.assets.precompile += %w( search.js )
 
 	# Ignore bad email addresses and do not raise email delivery errors.
 	# Set this to true and configure the email server for immediate delivery to raise delivery errors.
