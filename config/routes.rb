9f81227daa90cd547da32c753c6315b6f7f22c1a
fai.weiner <fai.weiner@gmail.com>
2015-01-28 19:25:17 +1100

    removed extra scheme_list api from routes

--- config/routes.rb
+++ config/routes.rb
@@ -88,18 +88,9 @@ Rails.application.routes.draw do
 	get		'admin/seasons' => 'admin#seasons', :as => :admin_seasons
 
 	# API for pulling lists via AJAX
-<<<<<<< HEAD
-	get		'api/shows'				=> 'application#shows_list',				:as => :api_shows
-	get		'api/scheme_types' => 'application#scheme_types_list', :as => :api_scheme_types
-	get		'api/schemes'			=> 'application#schemes_list',			:as => :api_schemes
-	get		'api/seasons'			=> 'application#seasons_list',			:as => :api_seasons
-	get		'api/episodes'		=> 'application#episodes_list', 		:as => :api_episodes
-	get		'api/contestants'	=> 'application#contestants_list',	:as => :api_contestants
-=======
 	get		'api/shows'				=> 'application#shows_list',			:as => :api_shows
 	get		'api/seasons'			=> 'application#seasons_list',		:as => :api_seasons
 	get		'api/episodes'			=> 'application#episodes_list', :as => :api_episodes
 	get		'api/contestants'	=> 'application#contestants_list', :as => :api_contestants
 	get		'api/schemes' => 'application#schemes_list', 				:as => :api_schemes
->>>>>>> adminBuildOut
 end
