3210b1740fb223da3b884169c0a3f0130618ede6
fai.weiner <fai.weiner@gmail.com>
2015-01-30 21:58:52 +1100

    restore schemes_controller to last state at8pm

--- app/controllers/schemes_controller.rb
+++ app/controllers/schemes_controller.rb
@@ -1,45 +1,113 @@
 class SchemesController < ApplicationController
 	layout "admin"
 	def index
-		@shows = Show.all.order(name: :desc)	
-		@selected = Scheme.all.order(type: :asc, points_asgn: :asc, description: :asc, show_id: :asc)
+		@shows = Show.all.order(name: :asc)	
+		@selected = Scheme.all.order(points_asgn: :asc, description: :asc, show_id: :asc)
 		@scheme = Scheme.new
 	end
 
 	def display_all
-		@selected = Scheme.all.order(type: :asc, points_asgn: :asc, description: :asc, show_id: :asc)
+		@selected = Scheme.all.order(points_asgn: :asc, description: :asc, show_id: :asc)
 		respond_to do |format|
-				format.js
+			format.js
 		end
 	end
 
 	def create
 		@scheme = Scheme.new scheme_params
-		if @scheme.save
-			@selected = Scheme.where(:show_id => params[:show_id]).order(type: :asc, points_asgn: :asc, description: :asc)
+		if @scheme.save	
+			@selected = Scheme.where(:show_id => params[:show_id]).order(points_asgn: :asc, description: :asc)	
+			flash[:notice] = "New scheme has been successfully added."
+			flash[:color] = "alert-success success"
 			respond_to do |format|
-					format.js
-			end			
-		else
-			raise
-			flash[:notice] = "Something went wrong and the scheme was not saved."
-			flahs[:color] = "alert-warning warning"
+				format.json {
+					render :json => {
+						:schemeList => @selected,
+						:notice => flash[:notice],
+						:color => flash[:color]
+					}
+				}
+			end
+		elsif @scheme.errors
+			flash[:notice] = "Invalid - Please fix the below before submitting:"
+			flash[:color] = "alert-warning warning"
+			respond_to do |format|
+				format.json {
+					render :json => {
+						:errors => @scheme.errors.full_messages,
+						:notice => flash[:notice],
+						:color => flash[:color],
+						:success => false
+					}, :status => 400
+				}
+			end
 		end
-		raise
+	end
+
+	def edit
+		@scheme = Scheme.find(params[:id])
+		respond_to do |format|
+			format.json {
+				render :json => {
+					:scheme => @scheme,
+				}, :status => 200
+			}
+		end
+	end
+
+	def update
+		# @scheme = Scheme.find(params[:id])
+		# if params[:type_text].present?
+		# 	type = params[:type_text]
+		# else
+		# 	schemeType = params[:type_select]
+		# end
+		# if @scheme.update(type: schemeType, description: params[:description], points_asgn: params[:points_asgn])
+		# 	flash[:notice] = "Scheme has been successfully updated."
+		# 	flash[:color] = "alert-success success"
+		# else
+		# 	@scheme.errors
+		# 	flash[:notice] = "Something went wrong and the scheme was not updated."
+		# 	flash[:color] = "alert-warning warning"
+		# end
 	end
 
 	def from_show
-		@selected = Scheme.where(:show_id => params[:show_id]).order(type: :asc, points_asgn: :asc, description: :asc)
+		@selected = Scheme.where(:show_id => params[:show_id]).order(points_asgn: :asc, description: :asc)
 		respond_to do |format|
 				format.js
 		end
 	end
 
-
+	def destroy
+		@scheme = Scheme.find params[:id]
+		if	@scheme.destroy
+			flash[:notice] = "Scheme\##{@scheme.id} has been successfully deleted."
+			flash[:color] = "alert-success"
+		else
+			flash[:notice] = "Something went wrong, please try again."
+			flash[:color] = "alert-danger"
+		end
+		redirect_to schemes_path
+	end
 
 	private
 
 	def scheme_params
-		params.require(:scheme).permit(:id, :type, :show_id, :description, :points_asgn)
+		if params[:scheme][:type_text].present?
+			params[:scheme][:type] = params[:scheme][:type_text]
+		elsif params[:scheme][:type_select].present?
+			case params[:scheme][:type_select]
+			when "Select type"
+				params[:scheme][:type] = nil
+			when "Add new type"
+				params[:scheme][:type] = nil
+			else
+				params[:scheme][:type] = params[:scheme][:type_select]
+			end
+		else
+			params[:scheme][:type]
+		end
+		params.require(:scheme).permit(:id, :show_id, :description, :points_asgn)
 	end
 end
