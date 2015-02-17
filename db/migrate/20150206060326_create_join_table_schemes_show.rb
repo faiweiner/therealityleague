da481dec76b60855bdccb1fe510ed2349d2f643d
fai.weiner <fai.weiner@gmail.com>
2015-02-17 13:19:22 +1100

    removed contestants_shows migration, issues with current

--- db/migrate/20150206060326_create_join_table_schemes_show.rb
+++
@@ -1,8 +0,0 @@
-class CreateJoinTableSchemesShow < ActiveRecord::Migration
-  def change
-    create_join_table :schemes, :shows, id: false do |t|
-    	t.integer :scheme_id
-    	t.integer :show_id
-    end
-  end
-end
